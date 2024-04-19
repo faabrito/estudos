e539a96da5e9177418e669edb194abca47aa9017
FERNANDO DE BRITO <109520329+faabrito@users.noreply.github.com>
2024-04-19 07:15:40 -0300

    Add files via upload
    estudos em py

---
+++ c.v. python/6.1.py
@@ -0,0 +1,6 @@
+n = int(input('digite um numero: '))
+d = n * 2
+t = n * 3
+r =  n** (1/2)
+print( ' o dobro de {} vale {}.'.format(n, d))
+print('O triplo de {} vale {}. \nA raiz quadrada de {} é igual a {:.2f}.'.format(n, (n*3), n, pow(n, (1/2))))
